184088029b3cb3a06b0ac56618793f30b2d42a64
Kieran Williames <kswilliames@gmail.com>
2020-05-29 18:39:37 +1000

    test more directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,6 +38,10 @@ echo "HOME $HOME"
 echo "ls HOME: "$(ls -la $HOME)
 echo "ls ~: "$(ls -la ~)
 echo "Connecting to $INPUT_REMOTE_HOST..."
+"ls -la ~/.docker"
+ls -la ~/.docker
+"ls -la $HOME/.docker"
+ls -la $HOME/.docker
 "cat ~/.docker/config.json"
 cat ~/.docker/config.json
 "cat $HOME/.docker/config.json"
